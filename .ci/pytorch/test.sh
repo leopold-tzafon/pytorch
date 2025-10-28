a1f7eaa51209fa06c08e8d11411adec435d9383c
Ting Lu <tingl@nvidia.com>
2025-10-27 18:17:17 -0700

    Do not skip torchrec

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -1757,8 +1757,7 @@ elif [[ "${TEST_CONFIG}" == *torchbench* ]]; then
   else
     # Do this after checkout_install_torchbench to ensure we clobber any
     # nightlies that torchbench may pull in
-    # Skip torchrec/fbgemm for cuda13 as they're not compatible yet
-    if [[ "${TEST_CONFIG}" != *cpu* && "${BUILD_ENVIRONMENT}" != *cuda13* ]]; then
+    if [[ "${TEST_CONFIG}" != *cpu* ]]; then
       install_torchrec_and_fbgemm
     fi
     PYTHONPATH=/torchbench test_dynamo_benchmark torchbench "$id"
